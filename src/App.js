f53c323b21750b3eb388a1c0b30ec63ccbc7fcde
chung-code <qc160@naver.com>
2021-03-15 22:08:39 +0900

    map

--- src/App.js
+++ src/App.js
@@ -2,22 +2,53 @@ import React, { Component } from 'react';
 import Customer from './components/Customer'
 import './App.css';
 
-const customer = {
+const customers = [
+{
+  'id': 1,
+  'image' : 'https://placeimg.com/64/64/any',
   'name' : '나동빈',
   'birthday' : '961222',
   'gender' : '남자',
   'job' : '대학생'
+},
+{
+  'id': 2,
+  'image' : 'https://placeimg.com/64/64/2',
+  'name' : '이동찬',
+  'birthday' : '960101',
+  'gender' : '남자',
+  'job' : '디자이너'
+},
+{
+  'id': 3,
+  'image' : 'https://placeimg.com/64/64/3',
+  'name' : '신충현',
+  'birthday' : '960222',
+  'gender' : '남자',
+  'job' : '개발자'
 }
+]
 
 class App extends Component {
   render() {
     return (
-      <Customer
-        name={customer.name}
-        birthday={customer.birthday}
-        gender={customer.gender}
-        job={customer.job}
-      />
+      <div>
+        {
+          customers.map(c => {
+            return (
+              <Customer
+                key={c.id}
+                id={c.id}
+                image={c.image}
+                name={c.name}
+                birthday={c.birthday}
+                gender={c.gender}
+                job={c.job}
+              />
+            );
+          })
+        }
+      </div>        
     );
   }
 }
